2e876a08ba14b05b28d00d7748fa2944d17c07d6
Raph Terrier <r4ph@me.com>
2022-09-29 17:01:16 +0200

    server cleanup

--- src/index.js
+++ src/index.js
@@ -1,27 +1,25 @@
-const { ApolloServer,gql } = require('apollo-server');
-const {readFileSync} = require('fs');
+const { ApolloServer, gql } = require('apollo-server');
+const { readFileSync } = require('fs');
 const path = require("path");
-const typeDefs = gql(readFileSync(path.resolve(__dirname, "./schema.graphql"), {encoding: 'utf-8'}));
-const {buildSubgraphSchema} = require('@apollo/subgraph');
+const typeDefs = gql(readFileSync(path.resolve(__dirname, "./schema.graphql"), { encoding: 'utf-8' }));
+const { buildSubgraphSchema } = require('@apollo/subgraph');
 const resolvers = require('./resolvers');
 const TrackAPI = require('./datasources/track-api');
 
-async function startApolloServer(typeDefs, resolvers) {
-  const server = new ApolloServer({
-    schema: buildSubgraphSchema({typeDefs, resolvers}),
-    dataSources: () => {
-      return {
-        trackAPI: new TrackAPI(),
-      };
-    },
-  });
+const server = new ApolloServer({
+  schema: buildSubgraphSchema({ typeDefs, resolvers }),
+  dataSources: () => {
+    return {
+      trackAPI: new TrackAPI(),
+    };
+  },
+});
 
-  const { url, port } = await server.listen({ port: process.env.PORT || 4001 });
+server.listen({ port: process.env.PORT || 4001 }).then(({ url, port }) => {
   console.log(`
-      🚀  Server is running
-      🔉  Listening on port ${port}
-      📭  Query at ${url}
-    `);
-}
+    🚀  Server is running
+    🔉  Listening on port ${port}
+    📭  Query at ${url}
+  `);
+});
 
-startApolloServer(typeDefs, resolvers);
